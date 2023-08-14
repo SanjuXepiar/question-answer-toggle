84f9cf961cdac1b287ca4219b4785e5376c878bb
SanjuXepiar <sanjusarkar238@gmail.com>
2023-08-14 21:02:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import data from "./Data";
 // The Question component has been called as SingleQuestion as it has been exported as default.
 import SingleQuestions from "./Questions";
 function App() {
-  const [questions, setQuestions] = useState(data);
+  const questions = data;
   return (
     <div className="main">
-      <h2>Question and Answers!</h2>
+      <h2>Questions and Answers!</h2>
       <section>
         {questions.map((question) => {
           return <SingleQuestions key={question.id} {...question} />;
